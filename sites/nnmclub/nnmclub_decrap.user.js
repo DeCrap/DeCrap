8b77b8da4864b38f2ed8357c705970c6f21838fb
DeCrap <V0xN3t@gmail.com>
2016-07-27 03:23:31 +0300

    add logs, settings

--- sites/nnmclub/nnmclub_decrap.user.js
+++ sites/nnmclub/nnmclub_decrap.user.js
@@ -1,7 +1,6 @@
 // ==UserScript==
 // @name         NNM Club DeCrap
 // @namespace    http://tampermonkey.net/
-// @version      0.2.2
 // @downloadURL  https://github.com/DeCrap/DeCrap/raw/master/sites/nnmclub/nnmclub_decrap.user.js
 // @description  Removes all crap and shit for this site
 // @author       DeCrap
@@ -11,25 +10,37 @@
 // ==/UserScript==
 
 //Docs for "one hour learning" antiadvertisment scripting, lol
+//script header: https://wiki.greasespot.net/Metadata_Block
 //Xpath selector format: http://www.w3schools.com/xsl/xpath_syntax.asp, https://ru.wikipedia.org/wiki/XPath
 //html: http://htmlbook.ru, https://webref.ru/html
 //js: https://learn.javascript.ru
 
 function filters()
 {
+    log('filters fill');
     //del(filter) - clear advertisment in realtime, before html-page comlete loading
     //adel(filter) - clear advertisment after html-page comlete loading
     //edit(filter, newvalue) - edit element in realtime, before html-page comlete loading
     //aedit(filter, newvalue) - edit element after html-page comlete loading
 
-    //remove adbloc detect
+    //adblock detect
     del('//div[@class="request"]');
     del('//script[contains(@src, "requests")]');
 
-    //remove directadvert.ru
-    del('//center/div[contains(@class, "DA-BLOCK")]');
-    adel('//center/script[contains(., "DA-BLOCK")]');
+    //directadvert.ru
+    del('//div[contains(@class, "DA-BLOCK")]');
+    adel('//script[contains(., "DA-BLOCK")]');
     del('//script[contains(@src, "directadvert.ru") or contains(., "directadvert.ru")]');
+    
+    //marketgid.com
+    del('//script[contains(@src, "marketgid.com") or contains(., "marketgid.com")]');
+    del('//div[@class="mgbox"]');
+    
+    //awesomeredirector.com
+    del('//*[contains(@href, "awesomeredirector.com")]');
+    
+    //background image
+    del('//style[contains(., "background-image")]');
 }
 
 
@@ -41,8 +52,10 @@ function filters()
 
 function main()
 {
+    log('main loop start');
     if (count > 0)
     {
+        log('realtime clearing executing');
         count = 0;
         remove(sync_filter);
         editor(sync_edit);
@@ -50,22 +63,29 @@ function main()
 
     if (count > 0)
     {
-        id = setTimeout(main, 100);
+        log('crap found: '+count+' elements, set start now');
+        id = setTimeout(main, timeout);
     }
 
     if (last)
     {
+        log('last block start');
+        log('freezing async script');
         ClearAllTimeouts();
+        log('last clearing execute');
         remove(sync_filter);
         remove(async_filter);
         editor(sync_edit);
         editor(async_edit);
         clearTimeout(id);
+        log('last block end');
     }
+    log('crap found in this cicle: '+count+' elements');
+    log('main loop end, sleeping');
 }
 
 
-
+log('starting');
 //begin script
 
 var last = false;
@@ -75,6 +95,8 @@ var sync_filter = [];
 var async_filter = [];
 var sync_edit = [];
 var async_edit = [];
+var timeout = 100;
+var debug = 0;
 
 //start script
 count++;
@@ -86,10 +108,10 @@ filters();
 document.onreadystatechange = wait_load; //wait complete loading and clear async crap
 
 //set interactive clear crap
-id = setTimeout(main, 100);
+id = setTimeout(main, timeout);
 
 //end.
-
+log('sleeping');
 
 
 //misc func
@@ -100,9 +122,11 @@ function remove(array)
     {
         for (let i = 0; i <= array.length - 1; i++)
         {
+            log('del("'+array[i]+'")');
             let result = document.evaluate(array[i], document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
             if ((result !== null) && (result.snapshotLength > 0))
             {
+                log('found '+result.snapshotLength+' elements');
                 for (let i = 0; i <= result.snapshotLength - 1; i++)
                 {
                     let element = result.snapshotItem(i);
@@ -116,7 +140,15 @@ function remove(array)
 
 function editor()
 {
-    
+    log('editing');
+}
+
+function log(str)
+{
+    if (debug === 1)
+    {
+        console.log(Math.round(performance.now())+': DeCrap '+str);
+    }
 }
 
 function ClearAllIntervals() {
@@ -133,8 +165,9 @@ function wait_load()
 {
     if (document.readyState === "complete")
     {
+        log('site load completed');
         last = true;
-        id = setTimeout(main, 100);
+        id = setTimeout(main, timeout);
     }
 }
 
