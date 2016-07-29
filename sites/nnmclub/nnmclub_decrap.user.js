bff8c94f0f106030906af958cfb130a4568154ca
DeCrap <V0xN3t@gmail.com>
2016-07-29 03:33:09 +0300

    fix

--- sites/nnmclub/nnmclub_decrap.user.js
+++ sites/nnmclub/nnmclub_decrap.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NNM Club DeCrap
 // @description  Removes all crap and shit for this site
-// @version      0.4.0.5
+// @version      0.4.0.6
 // @downloadURL  https://github.com/DeCrap/DeCrap/raw/core/sites/nnmclub/nnmclub_decrap.user.js
 // @author       DeCrap
 // @license      GPL3
@@ -15,8 +15,9 @@
 (function() {
 	'use strict';
 	//var core = require('https://github.com/DeCrap/DeCrap/raw/core/core/v1/decrap_core_v1.user.js');
-	console.dir(Math.round(performance.now()) + ': DeCrap ' + 'nnm');
-	console.dir(Math.round(performance.now()) + ': DeCrap ' + log);
+	console.log(Math.round(performance.now()) + ': DeCrap ' + 'nnm');
+	var log = require('https://github.com/DeCrap/DeCrap/raw/core/core/v1/decrap_core_v1.user.js');
+	console.log(Math.round(performance.now()) + ': DeCrap ' + log('core'));
 
 	//log('nnmnnmnnm')
 	//var log = core.log;
